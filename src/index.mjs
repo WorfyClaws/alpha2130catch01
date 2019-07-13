37ae3fd7d629793ca8b202e271eeeb2828ed1679
Andrew Potter <apottere@gmail.com>
2019-07-13 02:21:40 -0400

    Automatically catch pokemon

--- src/index.mjs
+++ src/index.mjs
@@ -1,25 +1,73 @@
+import tf from '@tensorflow/tfjs-node';
 import Discord from 'discord.js';
+import request from 'request';
+
 import { token } from './token';
+import { Data } from './data';
+import { Model } from './model';
+import { modelDir } from './dirs';
 
+const data = new Data();
+const model = new Model();
 const client = new Discord.Client();
+
+const j2eChannelId = '598497945666453504';
 const channelId = '599371457771995149';
-let interval;
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    interval = client.setInterval(() => { client.channels.get(channelId).send('a'); }, 1500);
-});
+const getBufferForImageUrl = async (url) => {
+    return new Promise((resolve, reject) => {
+        request({url: url, encoding: null}, (error, response, data) => {
+            if(error != null || response.statusCode != 200) {
+                reject(new Error(`Error getting page from bulbapedia (${response.statusCode}, ${error}) for URL: ${url}`));
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
 
-client.on('message', (msg) => {
-    if(msg.channel.id === channelId && msg.author.username === 'Pokécord') {
-        const embeds = msg.embeds;
-        if(embeds && embeds.length > 0) {
-            const embed = embeds.find((e) => e.title.match(/A wild pok.mon has appeared!/));
-            if(embed) {
-                console.log(embed.image.url);
+const predict = async (tensor) => {
+    return new Promise((resolve, reject) => {
+        tf.tidy(() => {
+            resolve(model.getPrediction(tensor));
+        });
+    });
+};
+
+const getPredictionForUrl = async (url) => {
+    const buffer = await getBufferForImageUrl(url);
+    const tensor = await data.fileToTensor(buffer);
+    const prediction = await predict(tensor);
+
+    return prediction.label;
+};
+
+const run = async () => {
+    await model.init();
+    await model.loadModel(modelDir);
+
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        client.setInterval(() => { client.channels.get(channelId).send('a'); }, 2000);
+    });
+
+    client.on('message', (msg) => {
+        if((msg.channel.id === j2eChannelId || msg.channel.id === channelId) && msg.author.username === 'Pokécord') {
+            const embeds = msg.embeds;
+            if(embeds && embeds.length > 0) {
+                const embed = embeds.find((e) => e.title.match(/A wild pok.mon has appeared!/));
+                if(embed) {
+                    getPredictionForUrl(embed.image.url)
+                        .then(prediction => {
+                            msg.channel.send(`p!catch ${prediction.toLowerCase()}`);
+                        });
+                }
             }
         }
-    }
-});
+    });
 
-client.login(token);
+    client.login(token);
+
+};
+
+run();
