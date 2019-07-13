2051894e3278aa169e87f4b7d4f4355c9e8181b0
Andrew Potter <apottere@gmail.com>
2019-07-13 01:18:32 -0400

    Reorganize code

--- dirs.mjs
+++
@@ -1,4 +0,0 @@
-export const imageDir = 'images';
-export const modelDir = 'model';
-export const testsDir = 'tests';
-
---
+++ src/dirs.mjs
@@ -0,0 +1,8 @@
+import path from 'path';
+
+const basedir = path.join('.', 'data');
+
+export const imageDir = path.join(basedir, 'images');
+export const modelDir = path.join(basedir, 'model');
+export const testsDir = path.join(basedir, 'tests');
+
